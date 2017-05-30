6459ec1a91b7a5751526844237b2e990e000cdde
Cewei Cui <cewei.cui@gmail.com>
2017-05-30 02:25:15 -0700

    scaffold code 2

--- app/models/photopost.rb
+++ app/models/photopost.rb
@@ -1,2 +1,11 @@
 class Photopost < ApplicationRecord
+	validates :image, presence: true
+
+	has_attached_file :image, styles: { 
+		:thumb => "100x100#",
+		:small => "200x200#",
+		:medium => "640x640#" }
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	
 end
+
